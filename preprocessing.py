cbbd7793687fe930d2bbf226ed66eee77d13b99e
paozer <p.ramellaratin@gmail.com>
2018-11-19 21:37:16 +0100

    made function work - clean up

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,10 @@
-import nltk
 from nltk.corpus import stopwords
-# from nltk.corpus.wordnet import ADJ, NOUN, VERB, ADJ
-from textblob.wordnet import ADJ, NOUN, VERB, ADJ
 from nltk.stem import WordNetLemmatizer
-import contractions
-import os
-import string
+from nltk import word_tokenize, pos_tag
+from textblob.wordnet import ADV, NOUN, VERB, ADJ
+from contractions import fix
 
-f = open('/Users/paozer/Documents/patent-classification/AU0000046_03082000.txt', 'r+', encoding='utf-8')
+f = open('/Users/paozer/Documents/patent-classification/AU9700836_18061998.txt', 'r+', encoding='utf-8')
 
 ipc = f.readline().split()
 text = f.read()
@@ -15,16 +12,14 @@ text = f.read()
 f.close()
 
 # remove brakets 
-# remove contractions
-def replace_contractions(text):
-	return contractions.fix(text)
 
-# tokenizing
-def tokenise_text(text):
-	return nltk.word_tokenize(text)
+def replace_contractions(text): #works
+	return fix(text)
 
-# remove numbers
-def remove_numbers(words):
+def tokenise_text(text): #works
+	return word_tokenize(text)
+
+def remove_numbers(words): #works
 	new_words = []
 	for word in words:
 		if not word.isdigit():
@@ -33,78 +28,93 @@ def remove_numbers(words):
 			pass
 
 	return new_words
-	
-# stop words
-def remove_stopwords(words):
+
+def remove_stopwords(words): #works
 	new_words = []
 	for word in words:
 		if word not in stopwords.words('english'):
 			new_words.append(word)
 		else:
-			pass
+			continue
 
-# evtl. remove non ascii 
-# remove punctuation
-def remove_punctuation(words):
-	new_words = []
-	for word in words:
-		foo = word.translate(None, string.punctuation)
-		if foo != '':
-			new_words.append(foo)
 	return new_words
 
-# lower case 
+# evtl. remove non ascii 
+
+def remove_punctuation(words): # works maybe improve
+	new_words = []
+	punctuation = ['.', ',', '?', '!', ';', "'", '"', ':', '(', ')']
+
+	for word in words:		
+		if word not in punctuation:
+			new_words.append(word)
+	
+	return new_words
+
 def to_lowercase(words):
 	new_words = []
+
 	for word in words:
-		new_words.apppend(word.lower())
+		new_words.append(word.lower())
+	
 	return new_words
 
 # lemmatization
-def lemmatize(words):
-	new_words = []
-
-	tag_words = nltk.pos_tag(words)
-	lemmatizer = WordNetLemmatizer()
+def lemmatize(words): # need to investigate words which throw KeyError
 	
+	tag_words = pos_tag(words) # seems to work correctly
+
 	dic = {
-		'JJ': ADJ, 
+		'JJ': ADJ,
 		'JJR': ADJ, 
 		'JJS': ADJ, 
+
 		'RB': ADV, 
 		'RBR': ADV, 
 		'RBS': ADV,
-	    'NN': NOUN,
-	    'NNP': NOUN,
-	    'NNS': NOUN,
-	    'NNPS': NOUN,
-	    'VB': VERB,
-	    'VBG': VERB,
-	    'VBD': VERB,
-	    'VBN': VERB,
-	    'VBP': VERB,
-	    'VBZ': VERB
-	    }
+
+		'NN': NOUN,
+		'NNP': NOUN,
+		'NNS': NOUN,
+		'NNPS': NOUN,
+
+		'VB': VERB,
+		'VBG': VERB,
+		'VBD': VERB,
+		'VBN': VERB,
+		'VBP': VERB,
+		'VBZ': VERB
+	}
+
+	lemmatizer = WordNetLemmatizer()
+	new_words = []
 
 	for word in tag_words:
-		
 		tag = word[1]
+		
 		try:
 			new_tag = dic[tag]
-			foo = lemmatizer.lemmatize(word[0], new_tag)
-			new_words.append(foo)
+			lemmatized_word = lemmatizer.lemmatize(word[0], new_tag)
+			new_words.append(lemmatized_word)
 
 		except KeyError:
 			print('KeyError on tag @ ', word[0])
 		
 	return new_words
 
+print("> removing contractions")
 text = replace_contractions(text)
+print("> tokenising text")
 words = tokenise_text(text)
+print("> removing numbers")
 words = remove_numbers(words)
+print("> removing stopwords")
 words = remove_stopwords(words)
+print("> remove punctuation")
 words = remove_punctuation(words)
+print("> to lowercase")
 words = to_lowercase(words)
+print("> lemmatizing")
 words = lemmatize(words)
-
+input(">")
 print(words)
